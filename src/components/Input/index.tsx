872fffede6b6375b5b9bbb7a170fc9539ea1877c
vitebo <vitebo@hotmail.com>
2021-04-10 22:04:42 -0300

    refactor(components/input): use typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,9 +9,16 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder?: string;
+  icon?: React.ElementType<{
+    size: number;
+  }>,
+}
 
+const Input = ({ name, icon: Icon, placeholder, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
@@ -23,7 +30,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
@@ -41,6 +47,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
       <input
         onFocus={handleInputFocus}
+        placeholder={placeholder}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
